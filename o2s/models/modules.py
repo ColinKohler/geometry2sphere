3f95032f316bca88aa8b67dfbd221dbd2daf3a01
Colin Kohler <colink78@gmail.com>
2024-08-28 19:10:31 -0400

    Refactoring models. Renamed initial model as mesh2sphere as we might add different types of input2sphere.

--- o2s/models/modules.py
+++ o2s/models/modules.py
@@ -1,23 +1,132 @@
+""" modules.py """
+
+from typing import List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from e3nn import nn as enn
+from e3nn import o3
+from e3nn.nn import SO3Activation
+
+from o2s.models import e3nn_utils
+
 
 class MLP(nn.Module):
-    def __init__(self, hidden_dim, input_dim):
-        super(MLP, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.input_dim = input_dim
+    """Simple MLP."""
 
-        layers = []
-        for hid_dim in hidden_dim:
-            layers.append(nn.Linear(in_features=input_dim, out_features=hid_dim))
-            layers.append(nn.ReLU())
-            input_dim = hid_dim
+    def __init__(
+        self,
+        hiddens: List[int],
+        dropout: float = 0.0,
+        act_out: bool = True,
+    ):
+        super().__init__()
 
-        layers.append(nn.Linear(in_features=input_dim, out_features=self.input_dim))
+        layers = list()
+        for i, (h, h_) in enumerate(zip(hiddens, hiddens[1:])):
+            layers.append(nn.Linear(h, h_))
+            is_last_layer = i == len(hiddens) - 2
+            if not is_last_layer or act_out:
+                layers.append(nn.LeakyReLU(0.01, inplace=True))
+                layers.append(nn.Dropout(dropout))
+        self.mlp = nn.Sequential(*layers)
 
-        self.layers = nn.Sequential(*layers)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.mlp(x)
+
+
+class SO3Convolution(nn.Module):
+    """SO3 Convolutionl
+
+    Args:
+        f_in:
+        f_out:
+        lmax_in:
+        kernel_grid:
+        lmax_out:
+    """
+
+    def __init__(
+        self, f_in: int, f_out: int, lmax_in: int, kernel_grid, lmax_out: int = None
+    ):
+        super().__init__()
+        if lmax_out == None:
+            lmax_out = lmax_in
+        self.register_parameter(
+            "w", nn.Parameter(torch.randn(f_in, f_out, kernel_grid.shape[1]))
+        )  # [f_in, f_out, n_so3_pts]
+        self.register_parameter(
+            "D", nn.Parameter(e3nn_utils.flat_wigner(lmax_in, *kernel_grid))
+        )  # [n_so3_pts, psi]
+        self.lin = o3.Linear(
+            e3nn_utils.so3_irreps(lmax_in),
+            e3nn_utils.so3_irreps(lmax_out),
+            f_in=f_in,
+            f_out=f_out,
+            internal_weights=False,
+        )
 
     def forward(self, x):
-        return self.layers(x)
+        # S2 DFT (matrix mulitplication)
+        psi = torch.einsum("ni,xyn->xyi", self.D, self.w) / self.D.shape[0] ** 0.5
+        return self.lin(x, weight=psi)
+
+
+class SO3ToS2Convolution(nn.Module):
+    """SO3 to S2 Linear Layer"""
+
+    def __init__(self, f_in: int, f_out: int, lmax_in: int, lmax_out: int):
+        super().__init__()
+        self.lin = o3.Linear(
+            e3nn_utils.so3_irreps(lmax_in),
+            e3nn_utils.s2_irreps(lmax_out),
+            f_in=f_in,
+            f_out=f_out,
+        )
+
+    def forward(self, x):
+        return self.lin(x)
+
+
+class SphericalCNN(nn.Module):
+    """Spherical CNN.
+
+    Args:
+        lmax:
+        feat:
+    """
+
+    def __init__(self, lmax: List[int], feat: List[int], N: int = 8):
+        super().__init__()
+
+        grid_s2 = e3nn_utils.s2_near_identity_grid()
+        grid_so3 = e3nn_utils.so3_near_identity_grid()
+
+        layers: list = []
+        for i, (f, f_) in enumerate(zip(feat[:-1], feat[1:-1])):
+            layers.append(
+                e3nn_utils.SO3Convolution(
+                    f,
+                    f_,
+                    lmax_in=lmax[i],
+                    lmax_out=lmax[i + 1],
+                    kernel_grid=grid_so3,
+                )
+            )
+
+            layers.append(
+                SO3Activation(lmax[i + 1], lmax[i + 1], torch.relu, resolution=N)
+            )
+        self.so3_conv = nn.Sequential(*layers)
+
+        self.lin = e3nn_utils.SO3ToS2Convolution(
+            feat[-2], feat[-1], lmax_in=lmax[-2], lmax_out=lmax[-1], kernel_grid=grid_s2
+        )
+
+    def forward(self, x):
+        x = self.so3_conv(x)
+        x = self.lin(x)
+
+        return x
--- o2s/models/object_2_sphere.py
+++ o2s/models/object_2_sphere.py
@@ -6,73 +6,33 @@ import torch.nn.functional as F
 
 from e3nn import nn as enn
 from e3nn import o3
-from e3nn.nn import SO3Activation
 
 from torch_harmonics.spherical_harmonics import SphericalHarmonics
 
 from o2s.models import e3nn_utils
-from o2s.models.modules import MLP
+from o2s.models.modules import MLP, SphericalCNN
 from o2s.models.encoders.equiformerv2.equiformerv2 import Equiformerv2
 
 
-class SphericalCNN(nn.Module):
+class Mesh2Sphere(nn.Module):
+    """Mesh2Sphere model. Converts betweeen 3D object meshes to spherical signals.
+
+    Args:
+        latent_lmax:
+        ouput_lmax:
+        latent_feat_dim:
+        max_radius:
+        num_out_spheres:
+    """
+
     def __init__(
         self,
-        lmax_in,
-        lmax_out,
-        f_in,
-        f_out,
-        lmax_list=[10, 15, 20],
-    ):
-        super().__init__()
-
-        grid_s2 = e3nn_utils.s2_near_identity_grid()
-        grid_so3 = e3nn_utils.so3_near_identity_grid()
-
-        self.so3_conv1 = e3nn_utils.SO3Convolution(
-            f_in, 128, lmax_in=lmax_in, lmax_out=lmax_list[0], kernel_grid=grid_so3
-        )
-        self.act1 = SO3Activation(lmax_list[0], lmax_list[0], torch.relu, resolution=12)
-
-        self.so3_conv2 = e3nn_utils.SO3Convolution(
-            128, 256, lmax_in=lmax_list[0], lmax_out=lmax_list[1], kernel_grid=grid_so3
-        )
-        self.act2 = SO3Activation(lmax_list[1], lmax_list[1], torch.relu, resolution=12)
-
-        self.so3_conv3 = e3nn_utils.SO3Convolution(
-            256, 512, lmax_in=lmax_list[1], lmax_out=lmax_list[2], kernel_grid=grid_so3
-        )
-
-        # Output: Maps to 53 (rho_0, rho_1, rho_2, rho_3, ...) -> 53 S2 signals
-        self.act3 = SO3Activation(lmax_list[2], lmax_list[2], torch.relu, resolution=12)
-        self.lin = e3nn_utils.SO3ToS2Convolution(
-            512, f_out, lmax_in=lmax_list[2], lmax_out=lmax_out, kernel_grid=grid_s2
-        )
-
-    def forward(self, x):
-        x = self.so3_conv1(x)
-        x = self.act1(x)
-
-        x = self.so3_conv2(x)
-        x = self.act2(x)
-
-        x = self.so3_conv3(x)
-        x = self.act3(x)
-
-        x = self.lin(x)
-
-        return x
-
-
-class Object2Sphere(nn.Module):
-    def __init__(
-        self,
-        latent_lmax,
-        output_lmax,
-        latent_feat_dim,
-        max_radius,
-        num_out_spheres=1,
-        use_mlp=True,
+        latent_lmax: int,
+        output_lmax: int,
+        latent_feat_dim: int,
+        max_radius: float,
+        num_out_spheres: int = 1,
+        use_mlp: bool = True,
         mlp_hidden_dim=[256, 512, 512],
     ):
         super().__init__()
@@ -82,7 +42,6 @@ class Object2Sphere(nn.Module):
         self.latent_feat_dim = latent_feat_dim
         self.num_out_spheres = num_out_spheres
 
-        self.irreps_in = o3.Irreps("1x0e")
         self.irreps_latent = e3nn_utils.so3_irreps(latent_lmax)
         self.irreps_enc_out = o3.Irreps(
             [
@@ -105,9 +64,7 @@ class Object2Sphere(nn.Module):
 
     def forward(self, x):
         batch_size = x.batch.max() + 1
-        enc_out = self.encoder(x)
-
-        z = self.linear_layers(enc_out.view(batch_size, 1, -1))
+        z = self.encoder(x)
         w = self.spherical_cnn(z)
         out = self.sh(w)
 
@@ -115,5 +72,5 @@ class Object2Sphere(nn.Module):
 
 
 if __name__ == "__main__":
-    o2s = Object2Sphere(2, 10, 32, 1.5)
-    print(type(o2s))
+    m2s = Mesh2Sphere(2, 10, 32, 1.5)
+    print(type(m2s))
